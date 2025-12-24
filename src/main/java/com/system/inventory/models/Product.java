0dfe8b49975fa3453ed89d7326698476f4dafc87
jeseniaisabel231 <130105827+jeseniaisabel231@users.noreply.github.com>
2025-12-23 21:05:20 -0500

    comitee

--- src/main/java/com/system/inventory/models/Product.java
+++ src/main/java/com/system/inventory/models/Product.java
@@ -24,7 +24,7 @@ public class Product {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(nullable = false, unique = true, length = 7)
+  @Column(nullable = false, unique = true)
   private String code;
 
   @Column(nullable = false, unique = true)
